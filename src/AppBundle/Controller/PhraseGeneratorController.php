50fe7e1adc881600e72027120f933d920f5ffa34
vnilov <nilov.vadim@gmail.com>
2016-08-14 21:56:06 +0300

    phrases init

--- src/AppBundle/Controller/PhraseGeneratorController.php
+++ src/AppBundle/Controller/PhraseGeneratorController.php
@@ -4,18 +4,27 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class PhraseGeneratorController
+class PhraseGeneratorController extends Controller
 {
-    /*
-     * @Route("/phrase_generator")
+    /**
+     * @Route("/phrase_generator", name="get_all_phrases")
+     * @Method("GET")
      */
     public function getPhrasesAction()
     {
-        return new Response(
-          '<html><body>Test</body></html>'
-        );
+        return $this->render('/phrase_generator/all.html.twig');
     }
 
+    /**
+     * @Route("/phrase_generator/{id}", name="get_one_phrase", requirements={"id": "\d+"})
+     * @Method("GET")
+     */
+    public function getPhrase()
+    {
+
+    }
 }
